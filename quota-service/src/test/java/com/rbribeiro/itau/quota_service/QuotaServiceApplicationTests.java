dfdac293a4ee38340fb11971f9ab34730cd5b093
RBRibeiro00 <romuloribeirob@hotmail.com>
2025-07-21 21:55:14 -0300

    Initial config

---
+++ quota-service/src/test/java/com/rbribeiro/itau/quota_service/QuotaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbribeiro.itau.quota_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
